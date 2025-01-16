445ea659e1a85e08a1ae4e8a1867e69b0dccee0e
mixa <m_kuznetcov07@mail.ru>
2025-01-16 19:21:50 +0300

    rewrote makemove and acceptgame methods

--- src/main/java/org/churka/ChessTUI.java
+++ src/main/java/org/churka/ChessTUI.java
@@ -1,5 +1,8 @@
 package org.churka;
 
+import com.github.bhlangonijr.chesslib.Side;
+import com.github.bhlangonijr.chesslib.Square;
+import com.github.bhlangonijr.chesslib.move.Move;
 import net.minecraft.client.multiplayer.PlayerInfo;
 import org.rusherhack.client.api.events.client.chat.EventAddChat;
 import org.rusherhack.client.api.feature.command.ModuleCommand;
@@ -28,6 +31,8 @@ public class ChessTUI extends ToggleableModule {
      */
     int cAccept;
     private String oppmaybe;
+    private State state;
+
     @Override
     public ModuleCommand createCommand(){
         return new ModuleCommand(this){
@@ -35,29 +40,51 @@ public class ChessTUI extends ToggleableModule {
             @CommandExecutor
             @CommandExecutor.Argument({"opponent"})
             public String createGame(String opponent){
-                if (!online().contains(opponent)) return "Player not found";
+                if (!onlinePlayers().contains(opponent)) return "Player not found";
                 mc.getConnection().sendCommand("w " + opponent + " CHESSREQUEST");
                 cAccept=chat.getValue();
                 return "Sent request";
             }
 
+            @CommandExecutor
+            public String acceptGame(){
+                if (oppmaybe==null) return "No game requests";
+                state=new State(oppmaybe,Side.BLACK);
+                state.board.setSideToMove(Side.BLACK);
+                return "Game accepted";
+            }
+
+            @CommandExecutor
+            @CommandExecutor.Argument({"from,to"})
+            public String makeMove(Square from, Square to){
+                if(!online()) return "Get on the server";
+                if(state.board.getSideToMove()!=state.myside) return "Not your time to move";
+                if(state.board.isMoveLegal(new Move(from,to),true)) return "Illigal move";
+                mc.getConnection().sendCommand("w "+state.getOpponent()+" MOVE "+from+" "+to);
+                return "Made move";
+            }
 
         };
     }
     @Subscribe
     public void onChat(EventAddChat event){
-        cAccept--;
+        if (cAccept==0) state=null;
         String msg=event.getChatComponent().getString();
         if (msg.contains("CHESSREQUEST")){
-            oppmaybe=msg.split("")[1];
+            oppmaybe=msg.split(" ")[1];
         }
     }
 
-    List<String> online(){
+    private List<String> onlinePlayers(){
         List<String> ls = List.of();
+        if(!online()) return ls;
         for (PlayerInfo pi: Objects.requireNonNull(mc.getConnection()).getOnlinePlayers()){
             ls.add(pi.getProfile().getName());
         }
         return ls;
+    }
+
+    boolean online(){
+        return mc.getConnection()!=null;
     }
 }
--- src/main/java/org/churka/State.java
+++ src/main/java/org/churka/State.java
@@ -1,11 +1,18 @@
 package org.churka;
 
+import com.github.bhlangonijr.chesslib.Board;
+import com.github.bhlangonijr.chesslib.Side;
+
 public class State {
-    public State(String opponent){
+    public State(String opponent,Side side){
         this.opponent=opponent;
+        this.myside=side;
+        this.board=new Board();
     }
 
     private final String opponent;
+    public Board board;
+    public Side myside;
 
     String getOpponent(){
         return opponent;
